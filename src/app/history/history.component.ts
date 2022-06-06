31254247e003ed4520bd6927badd80227b7311fc
Diego <diego.ordonez.becerril@gmail.com>
2022-06-06 15:25:43 -0500

    Add history page

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { History } from '../models/history';
+import { OrdersService } from '../services/orders.service';
 
 @Component({
   selector: 'app-history',
   templateUrl: './history.component.html',
-  styleUrls: ['./history.component.scss']
+  styleUrls: ['./history.component.scss'],
+  providers: [MessageService]
 })
 export class HistoryComponent implements OnInit {
+  public items: Array<History> = [];
 
-  constructor() { }
+  constructor(private ordersService: OrdersService, private messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.getHistory();
   }
 
+  private getHistory(){
+    this.ordersService.getHistory().subscribe(
+      data => {
+        this.items = data;
+      }, error => {
+        this.messageService.add({severity:'error', summary:'Error', detail:'Something was wrong', life:2000});
+      }
+    );
+  }
 }
